2ee6ed90daf87ea23bfef82e2fc26f77f5c0f123
paolomondelo <44991747+paolomondelo@users.noreply.github.com>
2022-12-13 16:33:30 +1300

    Update MoveMouse.py

--- MoveMouse.py
+++ MoveMouse.py
@@ -1,18 +1,6 @@
 import pyautogui
 import time
 
-# Set the time to stop moving the cursor (in 24-hour format)
-stop_hour = 18
-stop_minute = 30
-
 while True:
-    current_time = time.localtime()
-    current_hour = current_time.tm_hour
-    current_minute = current_time.tm_min
-
-    # Check if the current time is past the stop time
-    if current_hour > stop_hour or (current_hour == stop_hour and current_minute >= stop_minute):
-        break
-
     pyautogui.moveRel(100, 100)
-    time.sleep(180)
+    time.sleep(5)
